68811b01e8f5c52fc7d093aa26e6a1e3e422c47a
Morten Brudvik <morten@brudvik.com>
2019-01-25 14:34:22 +0100

    implemented netsh http add

--- UrlReserverConsole/Program.cs
+++ UrlReserverConsole/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace UrlReserverConsole
 {
@@ -10,6 +6,17 @@ namespace UrlReserverConsole
     {
         static void Main(string[] args)
         {
+            // netsh http add urlacl url=http://+9000/ user=everyone
+            const string address = "http://+:9000/";
+            var urlReserver = new UrlReserver();
+            var status = urlReserver.Reserve(address, "everyone");
+            Console.Out.WriteLine($"Trying to register \"{address}\" - { status }");
+
+            // netsh http delete urlacl url=http://+9000/
+
+            // netsh http show urlacl url=http://+9000/
+
         }
     }
+
 }
---
+++ UrlReserverConsole/UrlReserver.cs
@@ -0,0 +1,32 @@
+﻿using System.Diagnostics;
+
+namespace UrlReserverConsole
+{
+    public class UrlReserver
+    {
+        public void Reserve(string address, string user, string domain)
+        {
+            Reserve(address, $@"{domain}\{user}");
+        }
+        public bool Reserve(string address, string user)
+        {
+            var args = $@"http add urlacl url={address} user={user}";
+
+            var psi = new ProcessStartInfo("netsh", args)
+            {
+                Verb = "runas",
+                CreateNoWindow = true,
+                WindowStyle = ProcessWindowStyle.Hidden,
+                UseShellExecute = false,
+                RedirectStandardOutput = true
+            };
+
+            var process = Process.Start(psi);
+            var output = process.StandardOutput.ReadToEnd();
+                
+            process?.WaitForExit();
+
+            return output.Contains("URL reservation successfully added");
+        }
+    }
+}
